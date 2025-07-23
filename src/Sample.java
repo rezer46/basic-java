e17687792ecd317d8726188e10870b7f133e58a4
rezer46 <antonykj0519@gmail.com>
2025-07-23 13:24:05 +0530

    first commit

---
+++ src/Sample.java
@@ -0,0 +1,5 @@
+public class Sample {
+    public static void main(String[] args){
+        System.out.println("Hello! Welcome to the world of java.");
+    }
+}
---
+++ src/gettingstarted2.java
@@ -0,0 +1,5 @@
+public class gettingstarted2{
+    public static void main(String[] args){
+        System.out.println("Hello! Welcome to the world of java.");
+    }
+}
